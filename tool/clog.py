bdcf66fb000f63c8790a4a94cb5107d812d72d61
JackUnderwood <john.n.underwood@gmail.com>
2015-02-23 08:45:18 -0700

    Suppressed selenium debug logging to the WARNING level; added space before msg.

--- tool/clog.py
+++ tool/clog.py
@@ -2,12 +2,13 @@ __author__ = 'John Underwood'
 import logging
 
 from colorama import init, Fore, Back
+from selenium.webdriver.remote.remote_connection import LOGGER
 
 CRITICAL = 50
 ERROR = 40
 WARNING = 30
-TRACE = 25  # TODO: Add another level
 INFO = 20
+TRACE = 15  # TODO: Add another level
 DEBUG = 10
 
 
@@ -20,30 +21,31 @@ class CLog(logging.Logger):
     """
     def __init__(self, name="VTF", level=logging.INFO, log_name="vtf"):
         init()  # initialize colorama
+        LOGGER.setLevel(logging.WARNING)
         logging.Logger.__init__(self, name, level)
         logging.basicConfig(level=level)
         self.logger = logging.getLogger(log_name)
 
     def debug(self, msg, *args, **kwargs):
-        msg = Fore.GREEN + msg + Fore.RESET
+        msg = " " + Fore.GREEN + msg + Fore.RESET
         self.logger.debug(msg, *args, **kwargs)
 
     def info(self, msg, *args, **kwargs):
-        msg = Fore.CYAN + msg + Fore.RESET
+        msg = " " + Fore.CYAN + msg + Fore.RESET
         self.logger.info(msg, *args, **kwargs)
 
     def warning(self, msg, *args, **kwargs):
-        msg = Fore.YELLOW + msg + Fore.RESET
+        msg = " " + Fore.YELLOW + msg + Fore.RESET
         self.logger.warning(msg, *args, **kwargs)
 
     def error(self, msg, *args, **kwargs):
-        msg = Fore.RED + msg + Fore.RESET
+        msg = " " + Fore.RED + msg + Fore.RESET
         self.logger.error(msg, *args, **kwargs)
 
     def exception(self, msg, *args, **kwargs):
-        msg = Fore.RED + Back.YELLOW + msg + Fore.RESET + Back.RESET
+        msg = " " + Fore.RED + Back.YELLOW + msg + Fore.RESET + Back.RESET
         self.logger.exception(msg, *args, **kwargs)
 
     def critical(self, msg, *args, **kwargs):
-        msg = Fore.WHITE + Back.RED + msg + Fore.RESET + Back.RESET
+        msg = " " + Fore.WHITE + Back.RED + msg + Fore.RESET + Back.RESET
         self.logger.critical(msg, *args, **kwargs)
