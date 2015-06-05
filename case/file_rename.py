6aee4a0ba7e02ecb3478bdcb388f6cf5ec9d6e98
JackUnderwood <john.n.underwood@gmail.com>
2015-06-05 09:52:07 -0600

    Added new test case for File; moved file_basic to become a High level test case to be used by other files, such as file_rename.

---
+++ case/file_rename.py
@@ -0,0 +1,13 @@
+__author__ = 'John Underwood'
+
+from ui import UI
+from ui.low.file import File
+
+
+class FileRename(UI):
+    File()
+
+    runtime = {
+
+    }
+    pass
---
+++ ui/high/file_basic.py
@@ -0,0 +1,43 @@
+__author__ = 'John Underwood'
+"""
+This test uses the Chrome PDF Viewer, so it needs the following driver's option
+added; see UI() class: add_experimental_option('excludeSwitches', ['test-type'])
+
+An infobar warning banner will display, "You are using an unsupported
+command-line flag: --ignore-certificate-errors. Stability and security will
+suffer."
+
+Alter the above to avoid the banner:
+add_experimental_option(
+    'excludeSwitches',
+    ['test-type', 'ignore-certificate-errors']
+)
+"""
+from ui import UI
+
+
+class FileBasic(UI):
+
+    def __init__(self, override=None):
+        super().__init__(override)
+        runtime = {
+            'category': ('Click', '//*[@id="vsubnav"]/div/div[2]/ul', ),
+            'selectCategory': (
+                'Click',
+                '//*[@id="vsubnav"]/div/div[2]/ul/ul/li[2]'
+            ),
+            'subcategory': ('Click', '//*[@id="vsubnav"]/div/div[3]/ul', ),
+            'selectSubcategory': (
+                'Click',
+                '//*[@id="vsubnav"]/div/div[3]/ul/ul/li[12]'
+            ),
+            'selectFile': (
+                'Click',
+                '//*[@id="fileList"]/div[2]/div/div[2]/div[2]'
+            )
+        }
+        process = UI()
+        process.update(runtime)
+        order = ('category', 'selectCategory', 'subcategory',
+                 'selectSubcategory', 'selectFile', )
+        process.execute(order)
