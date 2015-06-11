ebe0eee0d4381d94b977e08ca0067d30ffa5c513
JackUnderwood <john.n.underwood@gmail.com>
2015-06-11 07:51:36 -0600

    Added test case that checks that phone is successfully changed.

---
+++ case/edit_phone.py
@@ -0,0 +1,34 @@
+__author__ = 'John Underwood'
+
+from ui import UI
+from ui.low.license import License
+from ui.high.checklist import Checklist
+from ui.high.expand_ribbon import ExpandRibbon
+
+
+class EditPhone(UI):
+    License()
+    Checklist()
+    ExpandRibbon()
+
+    runtime = {
+        'phone': (
+            'Click',
+            '//*[@id="ribbon_form"]/ul/li/div[2]/div[3]/div[1]/a[4]/i',
+        ),
+        'editPhone': (
+            'Click',
+            '//*[@id="phoneGrid_grid"]/tbody/tr[1]/td[14]/a/i'
+        ),
+        'revisePhone': ('Type', '#phone', '8012251155'),
+        'save': ('Click', '//*[@button="save"]')
+    }
+    expected = 'Phone saved!'
+    process = UI()
+    process.update(runtime)
+    order = ('phone', 'editPhone', 'revisePhone', 'save')
+    process.execute(order)
+    process.wait(2)
+    process.results(expected)
+    process.wait(1)
+    process.teardown()
