0ba3c67296bba4b2b1f8b133983c01f5c86d7cc7
JackUnderwood <john.n.underwood@gmail.com>
2015-06-11 07:50:19 -0600

    Added new high level class that is used to expand the ribbon for a variety of test cases.

---
+++ ui/high/expand_ribbon.py
@@ -0,0 +1,23 @@
+__author__ = 'John Underwood'
+from ui import UI
+
+
+class ExpandRibbon(UI):
+    """
+    Pre-requirement: needs to display provider's Checklist page.
+
+    Test case expands the ribbon.
+    """
+    def __init__(self, override=None):
+        super().__init__()
+
+        runtime = {
+            'expandRibbon': (
+                'Click',
+                '//*[@id="ribbon_form"]/ul/li/div[1]/div/div[1]'
+            ),
+        }
+        process = UI(override)
+        process.update(runtime)
+        order = ('expandRibbon', )
+        process.execute(order)
