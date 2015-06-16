6e9abe977f81e93fab5b40301f8fc65b12d506ff
JackUnderwood <john.n.underwood@gmail.com>
2015-06-16 15:22:47 -0600

    Added a new test case for trial piece.

--- sandbox/testsplitter.py
+++ sandbox/testsplitter.py
@@ -6,9 +6,11 @@ import sandbox.splitter as splitter
 # Unit Test
 class TestSplitFunction(unittest.TestCase):
     def setUp(self):
+        """Executes after every test"""
         pass
 
     def tearDown(self):
+        """Executes after every test"""
         pass
 
     def test_simple_string(self):
@@ -24,8 +26,14 @@ class TestSplitFunction(unittest.TestCase):
         self.assertEqual(r, ['GOOG', '100', '490.90'])
 
     def test_delimiter_with_spaces(self):
-        r = splitter.split('GOOG, 100, 490.90', delimiter=',')
+        r = splitter.split('GOOG, 100,  490.90', delimiter=',')
         self.assertEqual(r, ['GOOG', '100', '490.90'])
+
+    def test_all_parameters(self):
+        r = splitter.split('GOOG, 100,  490.90',
+                           types=[str, int, float],
+                           delimiter=',')
+        self.assertEqual(r, ['GOOG', 100, 490.9])
 
 if __name__ is '__main__':
     unittest.main()
