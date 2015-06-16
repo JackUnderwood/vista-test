bcd107fa320102f25f06c24351bd0db815e75994
JackUnderwood <john.n.underwood@gmail.com>
2015-06-16 09:20:51 -0600

    Added file to test unit test stuff.

---
+++ sandbox/splitter.py
@@ -0,0 +1,10 @@
+__author__ = 'John Underwood'
+
+def split(line, types=None, delimiter=None):
+    """
+    :param line: string of text
+    :param types:
+    :param delimiter:
+    :return: list
+    """
+    pass
