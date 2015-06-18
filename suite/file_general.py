bf2e4b37091dcb4a8fad99849cfa53f4e3d2974d
JackUnderwood <john.n.underwood@gmail.com>
2015-06-18 15:30:09 -0600

    Added more testcases to the File test suite.

--- suite/file_general.py
+++ suite/file_general.py
@@ -6,9 +6,14 @@ from ui.low.file import File
 from ui.high.file_select import FileSelect
 
 
-class TestSuiteFoo(unittest.TestCase):
-    print(">> Inside TestSuiteFoo class")
+# Note: may want to put tests that use FileSelect class in a separate suite.
+class TestSuiteFile(unittest.TestCase):
+    print(">> Inside TestSuiteFile class")
     process = UI()
+
+    # def __init__(self, override=None):
+    #     super().__init__(override)
+    #     self.process = UI()
 
     def setUp(self):
         File()
@@ -42,6 +47,34 @@ class TestSuiteFoo(unittest.TestCase):
         result = self.process.results(expected)
         self.assertTrue(result, msg=expected)
 
+    def test_file_reassign(self):
+        print(">>> Inside function test_file_reassign()")
+        FileSelect()
+
+        runtime = {
+            'reassign': ('Click', '#reassign'),
+            'inputProvider': (
+                'Type',
+                '#reassigneSearchDescription',
+                'lambert matt st:wv'
+            ),
+            'selectProvider': ('Click', '#user_name'),
+            'category': ('Click', '//*[@id="reassignObjectContainer"]/ul'),
+            'catLicenses': ('Click', '//*[@title="Licenses"]'),
+            'subcategory': ('Click', '//*[@id="reassignCategoryContainer"]/ul'),
+            'subcatStateLicense': ('Click', '//*[@title="State License"]'),
+            'copy': ('Click', '#reassignCopy')
+        }
+
+        expected = "File move successful"
+        self.process.update(runtime)
+        order = ('reassign', 'inputProvider', 'selectProvider', 'category',
+                 'catLicenses', 'subcategory', 'subcatStateLicense', 'copy')
+        self.process.execute(order)
+        self.process.results(expected)
+        result = self.process.results(expected)
+        self.assertTrue(result, msg=expected)
+
     def test_file_delete(self):
         print(">>> Inside function test_file_delete()")
         FileSelect()
@@ -53,6 +86,26 @@ class TestSuiteFoo(unittest.TestCase):
         expected = "Status updated successfully"
         self.process.update(runtime)
         order = ('delete', 'deactivate')
+        self.process.execute(order)
+        result = self.process.results(expected)
+        self.assertTrue(result, msg=expected)
+
+    def test_file_rotate(self):
+        print(">>> Inside function test_file_rotate()")
+        FileSelect()
+
+        runtime = {
+            'rotate': ('Click', '#rotate'),
+            'selectPage': (
+                'Click',
+                '//*[@id="toolPanelContainer"]/div[2]/div/div/div[2]/div[2]',
+            ),
+            'rotateLeft': ('Click', '#rotateLeft'),
+        }
+
+        expected = "Rotate was successful"
+        self.process.update(runtime)
+        order = ('rotate', 'selectPage', 'rotateLeft', )
         self.process.execute(order)
         result = self.process.results(expected)
         self.assertTrue(result, msg=expected)
