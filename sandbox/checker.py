91f10a04ec62904ed30f22f63e36ada0f097ac87
JackUnderwood <john.n.underwood@gmail.com>
2015-07-17 12:13:36 -0600

    Added a set of new ./ui/low items to check.

--- sandbox/checker.py
+++ sandbox/checker.py
@@ -1,6 +1,9 @@
 __author__ = 'John Underwood'
 from ui import UI
-from ui.low.goals_team_revenue_bonus import GoalsTeamRevenueBonus
+# from ui.low.goals_team_revenue_bonus import GoalsTeamRevenueBonus
+# from ui.low.goals_commission_rates import GoalsCommissionRates
+# from ui.low.goals_commission_report import GoalsCommissionReport
+from ui.low.goals_sales_base import GoalsSalesBase
 
 class Check(UI):
     """
@@ -8,7 +11,18 @@ class Check(UI):
     Change the import statement above.
     Change the class call below.
     """
-    GoalsTeamRevenueBonus()
+    # GoalsTeamRevenueBonus()  # currently the page is blank
+    # UI().results("")
 
+    # GoalsCommissionRates()
+    # UI().results("Commission Rates")
+
+    # GoalsCommissionReport()
+    # UI().results("Commission Report")
+
+    GoalsSalesBase()
+    UI().results("Standard Sales Base")
+
+    # Reserve - do not alter anything below
     UI().wait(5)
     UI().teardown()
