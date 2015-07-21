38c46694f77ed51a3cf287bbae7737c59c0a31ea
JackUnderwood <john.n.underwood@gmail.com>
2015-07-21 13:25:48 -0600

    Added new cases for entity and edu through the ribbon; entity still needs work.

---
+++ case/add_education_ribbon.py
@@ -0,0 +1,42 @@
+__author__ = 'John Underwood'
+
+from ui import UI
+from ui.low.license import License
+from ui.high.checklist import Checklist
+from ui.high.expand_ribbon import ExpandRibbon
+
+
+class AddEducationRibbon(UI):
+    """ Add education through the ribbon.
+    """
+    License()
+    Checklist()
+    ExpandRibbon()
+
+    runtime = {
+        'education': ('Click',
+                      '//*[@id="ribbon_form"]/ul/li/div[2]/div[3]/div[2]/a[4]/i'),
+        'addEducation': ('Click', '//*[@id="educationGrid_form"]/a[1]'),
+        'findEducation': ('Type', '#education_entity_id_desc', 'Tulane'),
+        'selectEducation': ('Click', '#user_name'),
+        'useEducationCv': ('Click',
+                           '//*[@id="EducationEdit_form"]/div[4]/div[1]/label'),
+        'selectDegree': ('Select',
+                         '#education_degree_id', 'Bachelor of Science'),
+        'selectType': ('Select', '#education_type_id', 'Undergraduate'),
+        'startDate': ('Type', '#start_date', '09011992'),
+        'endDate': ('Type', '#end_date', '06011996'),
+        'save': ('Click', '//*[@id="EducationEdit_form"]/div[9]/a[1]')
+    }
+    expected = "Education saved"
+    process = UI()
+    process.update(runtime)
+    order = ('education', 'addEducation', 'findEducation', 'selectEducation',
+             'useEducationCv', 'selectDegree', 'selectType', 'startDate',
+             'endDate', 'save', )
+    process.execute(order)
+    process.results(expected)
+    process.wait(3)
+    process.teardown()
+
+
--- case/add_entity.py
+++ case/add_entity.py
@@ -6,6 +6,10 @@ from ui.high.checklist import Checklist
 
 
 class AddEntity(UI):
+    """
+    The following are unable to access element: address, city, state,
+    zipcode, emailType, phoneType
+    """
     License()
     Checklist()
 
@@ -16,25 +20,27 @@ class AddEntity(UI):
         'name': ('Type', '#entity_name', 'Sierra Medicine'),
         'addressDesc': ('Type', '#address_description', 'Home Office'),
         'addressType': ('Select', '#address_correspondence_method_type_id', 'Work'),
-        'addrClick': ('Click', '#address_1'),
-        'address': ('Type', '#address_1', '123 State Street'),
+        'address': ('Type', '//*[@id="address_3"]', '123 State Street'),
         'city': ('Type', '#city', 'Portland'),
         'state': ('Select', '#state', 'Oregon'),
         'zipcode': ('Type', '#zip_code', '65432'),
         'email': ('Type', '#email_address', 'staff@sierramed.com'),
-        'emailType': ('Select', '#email_correspondence_method__type_id', 'Work'),
+        'emailType': ('Select', '#email_correspondence_method_type_id', 'Work'),
         'phone': ('Type', '#phone', '5051236789'),
         'phoneType': ('Select', 'phone_correspondence_method_type_id', 'Other'),
-        'save': ('Click', '//*[@button="save"]'),
+        'save': ('Click', '//*[@id="editEntityInformation_form"]/div[2]/a[1]'),
     }
 
     expected = "Information saved"
+    expected = "Cannot be blank"
     process = UI()
     process.update(runtime)
     order = ('experience', 'addExperience', 'addEntity', 'name', 'addressDesc',
-             'addressType', 'addrClick', 'address', 'city', 'state', 'zipcode', 'email',
-             'emailType', 'phone', 'phoneType', 'save'
-             )
+             'addressType', 'address', 'city', 'state', 'zipcode', 'email',
+             'emailType', 'phone', 'phoneType', 'save')
+    order = ('experience', 'addExperience', 'addEntity', 'name', 'addressDesc',
+             'addressType', 'address',
+             'email', 'save')
     process.execute(order)
     process.results(expected)
     process.wait(3)
