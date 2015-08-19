44956d6dcec4e954c76f3baf77b0576d611e305f
JackUnderwood <john.n.underwood@gmail.com>
2015-08-19 08:55:30 -0600

    Renamed sandbox file.

--- sandbox/splitter.py
+++ sandbox/splitter.py
@@ -11,7 +11,7 @@ def split(line, types=None, delimiter=None):
     element to its type, e.g. '100' with type int, converts element to 100
     Example - converts 'GOOG 100 490.90' using types [str, int, float] to
     a list ['GOOG', 100, 490.9].
-    See unit tests 'testsplitter.py' for more examples.
+    See unit tests 'splitter_test.py' for more examples.
     """
     fields = line.split(delimiter)
     # Clean out leading spaces for delimiter ['a, b, c'] -> ['a', ' b', ' c']
