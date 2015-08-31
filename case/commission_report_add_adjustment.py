e64c9dd4dc1bb07c21e4da8d5ed5bddd65abbdef
JackUnderwood <john.n.underwood@gmail.com>
2015-08-31 11:33:29 -0600

    Finished the test case for adding adjustments to commission reports; still needs to check the math (rate * adjustement).

--- case/commission_report_add_adjustment.py
+++ case/commission_report_add_adjustment.py
@@ -12,12 +12,32 @@ class ViewCommissionReport(UI):
         'result': ('Click', '//*[@associated-id="user_key_id_desc"]'),
         'month': ('Select', '#month', 'March'),
         'year': ('Select', '#year', '2015'),
-        
-    }
-    expected = ""
+        'addAdjustment': (
+            'Click', '//*[@id="revenue-table"]/tbody[2]/tr[9]/td/a'),
+        'assignNumber': (
+            'Type',
+            '//*[@id="revenue-table"]/tbody[2]/tr[9]/td[1]/input[2]',
+            '123456789'),
+        'findClient': (
+            'Type', '//*[@id="adj_client_id_desc"]', 'care'),
+        'selectCare': ('Click', '//*[@item_id="198206"]'),
+        'findProvider': (
+            'Type', '//*[@id="adj_provider_id_desc"]', 'matt lambert st:wv'),
+        'selectProvider': ('Click', '//*[@item_id="91273"]'),
+        'amount': (
+            'Type',
+            '//*[@id="revenue-table"]/tbody[2]/tr[9]/td[4]/input',
+            '25000'),
+        'save': (
+            'Click',
+            '//*[@id="revenue-table"]/tbody[2]/tr[9]/td[5]/a[1]/i'),
+    }  #
+    expected = "Saved adjustment"
     process = UI()
     process.update(runtime)
-    order = ()
+    order = ('find', 'result', 'month', 'year', 'addAdjustment',
+             'assignNumber', 'findClient', 'selectCare', 'findProvider',
+             'selectProvider', 'amount', 'save')
     process.execute(order)
     process.wait(2)
     process.results(expected)
