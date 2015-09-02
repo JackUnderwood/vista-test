57b4fdacbe6c46b030b9dc8ab412df7a0fbdb9bf
JackUnderwood <john.n.underwood@gmail.com>
2015-09-02 14:32:00 -0600

    Added a third-party names generator; the case is now functional.

--- case/regression/entity_add_ssn.py
+++ case/regression/entity_add_ssn.py
@@ -3,6 +3,7 @@ __author__ = 'John Underwood'
 import ui
 from ui import UI
 from tool.utilities import digits_only
+from tool.generators.names import get_full_name
 
 
 class EntityAddSsn(UI):
@@ -10,26 +11,35 @@ class EntityAddSsn(UI):
     Regression test for story #102370974
     """
     ssn = '123-12-1234'
+    full = get_full_name(gender='male')
+    name_list = full.split(' ')
+    first, last = name_list[0], name_list[1]
+    ui.log.info("First name: {0} & Last name: {1}".format(first, last,))
     runtime = {
-        'find': ('Type', '#main_desc', 'bert'),
+        'find': ('Type', '#main_desc', full),
         'addNewEntity': (
             'Click',
             '/html/body/header/nav/div/ul[1]/li[4]/div/div/div[4]/div/div[1]/a'),
         'entityType': ('Select', '#entity_type', 'Physician'),
-        'firstName': ('Type', '#entity_first_name', 'James'),
-        'lastName': ('Type', '#entity_name', 'Kramer'),
+        'firstName': ('Type', '#entity_first_name', first),
+        'lastName': ('Type', '#entity_name', last),
         'ssn': ('Type', '#ssn', digits_only(ssn)),
         'save': ('Click', '//*[@id="editEntityInformation_form"]/div[3]/a[1]'),
+        'select': ('Click', '//*[@id="user_name"]'),
+        'editEntity': (
+            'Click', '//*[@id="ribbon_form"]/ul/li/div[2]/div[1]/div[6]/a'),
     }
-
     process = UI()
     process.update(runtime)
     order = ('find', 'addNewEntity', 'entityType', 'firstName',
-             'lastName', 'ssn')
+             'lastName', 'ssn', 'save')
     process.execute(order)
     process.wait(3)
+    order = ('find', 'select', 'editEntity', )
+    process.execute(order)
     actual = process.get('#ssn', 'value')
-    ui.log.info('SSN value is {}'.format(actual,))
+    ui.log.info('SSN value is {}'.
+                format(actual if actual is not '' else 'None',))
     process.compare(ssn, actual)
     process.wait(5)
     process.teardown()
