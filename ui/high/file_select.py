e7cd5d2987552a833be68c2d91cc70cb92eee4f3
JackUnderwood <john.n.underwood@gmail.com>
2015-09-10 15:25:25 -0600

    Altered case to use the latest changes to File.

--- ui/high/file_select.py
+++ ui/high/file_select.py
@@ -1,46 +1,22 @@
 __author__ = 'John Underwood'
-"""
-This test uses the Chrome PDF Viewer, so it needs the following driver's option
-added; see UI() class: add_experimental_option('excludeSwitches', ['test-type'])
-
-An infobar warning banner will display, "You are using an unsupported
-command-line flag: --ignore-certificate-errors. Stability and security will
-suffer."
-
-Alter the above to avoid the banner:
-add_experimental_option(
-    'excludeSwitches',
-    ['test-type', 'ignore-certificate-errors']
-)
-"""
 from ui import UI
 
 
 class FileSelect(UI):
-    # May want to add placeholders for Category and Subcategory.
 
     def __init__(self, override=None):
         super().__init__()
         runtime = {
-            'category': ('Click', '//*[@id="vsubnav"]/div/div[2]/ul', ),
-            'cat': '2',
-            'selectCategory': (
-                'Click',
-                '//*[@id="vsubnav"]/div/div[2]/ul/ul/li[&cat;]'
-            ),
-            'subcategory': ('Click', '//*[@id="vsubnav"]/div/div[3]/ul', ),
-            'sub': '12',
-            'selectSubcategory': (
-                'Click',
-                '//*[@id="vsubnav"]/div/div[3]/ul/ul/li[&sub;]'
-            ),
+            'generic': ('Click', '//*[@id="vsubnav"]/div/div[6]/a[1]'),
+            'sub': '1',
+            'subcategory': (
+                'Click', '//*[@id="fileList"]/div[3]/div[1]/div[&sub;]'),
+            'line': '1',
             'selectFile': (
                 'Click',
-                '//*[@id="fileList"]/div[2]/div/div[2]/div[2]'
-            )
+                '//*[@id="fileList"]/div[3]/div[1]/div[&sub;]/div[&line;]'),
         }
         process = UI(override)
         process.update(runtime)
-        order = ('category', 'selectCategory', 'subcategory',
-                 'selectSubcategory', 'selectFile', )
+        order = ('generic', 'subcategory', 'selectFile', )
         process.execute(order)
