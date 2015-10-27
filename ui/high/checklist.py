f521fbe51e1c56ac7f906e139b00604c12df9a23
JackUnderwood <john.underwood@vistastaff.com>
2015-10-27 12:59:39 -0600

    Altered ui/high/checklist to include an 'entity' attribute, which other derived classes can use as a reference.

--- ui/high/checklist.py
+++ ui/high/checklist.py
@@ -9,12 +9,15 @@ class Checklist(UI):
 
     Test case goes into the Checklists.
     """
+    entity = '__OVERRIDE__'
+
     def __init__(self, override=None):
         super().__init__()
+        row = '1'  # Clicks on grid's first row.
 
         runtime = {
             'showAll': ('Click', '//*[@id="checklist-form-container"]/div[1]/a'),
-            'rowNum': '1',  # Clicks on grid's first row.
+            'rowNum': row,
             'provider': (
                 'Click',
                 '//*[@id="licenseRequestsGrid_grid"]/tbody/tr[&rowNum;]/td[4]/a',
@@ -22,5 +25,14 @@ class Checklist(UI):
         }
         process = UI(override)
         process.update(runtime)
-        order = ('showAll', 'provider', )
+        order = ('showAll', )
+        process.execute(order)
+
+        # Provides entity's name for derived classes
+        self.entity = process.get(
+            '//*[@id="licenseRequestsGrid_grid"]/tbody/tr['+row+']/td[4]/a',
+            'innerHTML')
+
+        process.update(runtime)
+        order = ('provider', )
         process.execute(order)
