ffe48f9fd43e7005adde63dc4fac667a0b43fc44
JackUnderwood <john.underwood@vistastaff.com>
2015-11-03 14:46:01 -0700

    Fixed regression test; now uses phone number as a unique identifier.

--- case/regression/entity_add_ssn.py
+++ case/regression/entity_add_ssn.py
@@ -2,6 +2,7 @@ import ui
 from ui import UI
 from tool.utilities import digits_only
 from tool.generators.generator import gen_name, gen_ssn, split_name
+from tool.generators.generator import gen_phone_number
 __author__ = 'John Underwood'
 
 
@@ -12,6 +13,8 @@ class EntityAddSsn(UI):
     ssn = gen_ssn()
     full = gen_name()
     first, last = split_name(full)
+    pn = gen_phone_number('Utah')
+    formatted_phone_number = pn[:3] + ' ' + pn[3:6] + '-' + pn[6:]
 
     ui.log.info("First name: {0} & Last name: {1}".format(first, last,))
     runtime = {
@@ -23,11 +26,15 @@ class EntityAddSsn(UI):
         'status': ('Select', '#entity_status', 'Possible'),
         'firstName': ('Type', '#entity_first_name', first),
         'lastName': ('Type', '#entity_name', last),
+        'phoneNum': ('Type', '#phone', pn),
+        'phoneType': ('Select', '#phone_correspondence_method_type_id', 'Work'),
         'ssn': ('Type', '#ssn', digits_only(ssn)),
         'fullName': full,
         'save': ('Click', '//*[@id="editEntityInformation_form"]/div[3]/a[1]'),
         # '//span[contains(@id, "user_name") and text()="&fullName;"]'
         # '//span[@id="user_name" and contains(., "&fullName;")]'
+        'findPhone': (  # e:123 333-4444
+            'Type', '#main_desc', 'p:' + formatted_phone_number),
         'select': ('Click', '//span[@id="user_name" and '
                             'contains(., "&fullName;")]'),
         'displaySsn': ('Click', '//*[@id="ribbon_form"]/ul/li/div[2]/div[1]/'
@@ -37,11 +44,11 @@ class EntityAddSsn(UI):
     process = UI()
     process.update(runtime)
     order = ('find', 'addNewEntity', 'entityType', 'status', 'firstName',
-             'lastName', 'ssn', 'save', )
+             'lastName', 'phoneNum', 'phoneType', 'ssn', 'save', )
     process.execute(order)
     process.wait(3)
 
-    order = ('find', 'select', 'displaySsn', 'continue', )
+    order = ('findPhone', 'select', 'displaySsn', 'continue', )
     process.execute(order)
     actual = process.get(
         '//*[@id="ribbon_form"]/ul/li/div[2]/div[1]/div[3]/span[1]',
