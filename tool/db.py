71179b8e5ecd1a56bb272b5c4a19d3e412c37bfc
JackUnderwood <john.n.underwood@gmail.com>
2015-11-06 14:09:25 -0700

    Added new tool file--tool.db--with get_record() function.

---
+++ tool/db.py
@@ -0,0 +1,32 @@
+import pymssql
+
+import tool.utilities as utils
+import book
+
+""" Everything to do with database access.
+"""
+__author__ = "John Underwood"
+
+
+def get_record(sql):
+    """
+    Takes in an SQL SELECT statement and returns a list of row(s)
+    :param sql: string - SELECT statement only
+    :return: list - result of row(s)
+    """
+    assert "SELECT" in sql
+    server = utils.get_configurations('SYSTEM', 'db_server')
+    username = utils.get_configurations('USER', 'db_user')
+    database = utils.get_configurations('SYSTEM', 'db_test')
+    conn = pymssql.connect(server, username, book.password, database)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+
+    row = cursor.fetchone()
+    result_set = []
+    while row:
+        result_set.append(row)
+        row = cursor.fetchone()
+
+    conn.close()
+    return result_set
