759d0e9c136d67b98d18f2a421b00d0c3db9438d
JackUnderwood <john.underwood@vistastaff.com>
2015-11-18 10:52:52 -0700

    Added database query using an entity's email address.

--- case/send_correspond.py
+++ case/send_correspond.py
@@ -1,13 +1,25 @@
-__author__ = 'John Underwood'
-
 from ui import UI
 from ui.low.license import License
 from ui.high.checklist import Checklist
 from ui.high.expand_ribbon import ExpandRibbon
 from ui.high.ribbon_corr import RibbonToCorrespondence
+from tool.db import get_record
+
+__author__ = 'John Underwood'
 
 
 class SendCorrespond(UI):
+    email = 'lambertmwl@gmail.com'
+    sql = """
+        SELECT ed.entity_id_number ID, ed.peoplesoft_name_on_check Name
+        FROM entity_details ed, email_address ea
+        WHERE ea.email_address = '{}'
+        AND ed.entity_id_number = ea.entity_id_number;
+    """.format(email, )
+    assert isinstance(sql, str)
+    record = get_record(sql)
+    item_id = record[0][0]
+
     License()
     Checklist()
     ExpandRibbon()
@@ -20,8 +32,8 @@ class SendCorrespond(UI):
     runtime = {
         'licenseStanding': ('Select', '#license_id', ),
         'entity': ('Click', '//*[@id="add-recipient-container"]/span[1]'),
-        'findEntity': ('Type', '<input>', 'matt lambert st:wv', ),
-        'selectEntity': ('Click', '//*[@item_id="91273"]'),
+        'findEntity': ('Type', '<input>', 'e:{}'.format(email, )),
+        'selectEntity': ('Click', '//*[@item_id="{}"]'.format(item_id,)),
         'checkAddress': ('Click', '//span[text()="Personal"]'),
         'saveDeliveryMethod': ('Click', '//a[@button="save"]', ),
         'cya': ('Click', '//body', ),
