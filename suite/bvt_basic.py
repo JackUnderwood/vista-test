65a618ceeabef335c97da3a20047f21f0a6245fb
JackUnderwood <john.underwood@vistastaff.com>
2015-11-30 10:35:42 -0700

    Added time delay to test_find to give ribbon time to display all of its elements; switched from ui.log.debug() to ui.log.info().

--- suite/bvt_basic.py
+++ suite/bvt_basic.py
@@ -8,7 +8,7 @@ __author__ = 'John Underwood'
 
 
 class BvtBasic(unittest.TestCase):
-    ui.log.debug(">> Inside BvtBasic class")
+    ui.log.info(">> Inside BvtBasic class")
 
     process = UI()
     debug = 'all'
@@ -28,7 +28,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'workspace' or debug is 'all', "testing {}".format(debug,))
     def test_workspace(self):
-        ui.log.debug('>>> Inside function test_workspace()')
+        ui.log.info('>>> Inside function test_workspace()')
 
         from ui.low.my_workspace import MyWorkspace
         MyWorkspace()
@@ -39,7 +39,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'advance' or debug is 'all', "testing {}".format(debug,))
     def test_advance_find(self):
-        ui.log.debug('>>> Inside function test_advance_find()')
+        ui.log.info('>>> Inside function test_advance_find()')
 
         from ui.low.advance_find import AdvanceFind
         AdvanceFind()
@@ -50,7 +50,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'find' or debug is 'all', "testing {}".format(debug,))
     def test_find(self):
-        ui.log.debug('>>> Inside function test_find()')
+        ui.log.info('>>> Inside function test_find()')
         expected = 'Find'
         runtime = {
             'find': ('Type', '#main_desc', 'Matt Lambert st:wv'),
@@ -59,15 +59,17 @@ class BvtBasic(unittest.TestCase):
                 'Click', '//*[@id="extended-results-body-91273"]/div[2]/a[1]')}
         self.process.update(runtime)
         self.process.execute(('find', 'select', ))
+        self.process.wait(2)  # ribbon needs time to display
         result = self.process.results(
             ' Matt Lambert', 'extended-results-body-91273')
+        self.process.wait(1)
         self.process.execute(('purge', ))
         self.assertTrue(result, msg=expected)
 
     @unittest.skipUnless(
         debug is 'modifiers' or debug is 'all', "testing {}".format(debug,))
     def test_modifiers(self):
-        ui.log.debug('>>> Inside function test_modifiers()')
+        ui.log.info('>>> Inside function test_modifiers()')
         expected = 'Available Modifiers'
         self.process.update({'modifiers': ('Click', '#modifierList'), })
         self.process.execute(('modifiers', ))
@@ -77,7 +79,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'suggestion' or debug is 'all', "testing {}".format(debug,))
     def test_suggestion_box(self):
-        ui.log.debug('>>> Inside function test_suggestion_box()')
+        ui.log.info('>>> Inside function test_suggestion_box()')
         expected = 'Suggestion Box'
         self.process.update(
             {'sbox': (
@@ -95,7 +97,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'create_notice' or debug is 'all', "testing {}".format(debug,))
     def test_create_notification(self):
-        ui.log.debug('>>> Inside function test_create_notification()')
+        ui.log.info('>>> Inside function test_create_notification()')
         expected = 'Create Notification'
         self.process.update(
             {'createNote': ('Click', '#remindMeButton'),
@@ -111,7 +113,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'directory' or debug is 'all', "testing {}".format(debug,))
     def test_employee_directory(self):
-        ui.log.debug('>>> Inside function test_employee_directory()')
+        ui.log.info('>>> Inside function test_employee_directory()')
         expected = 'Employee Directory'
         self.process.update(
             {'directory': ('Click', '#button_employee_directory'),
@@ -128,7 +130,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'notify' or debug is 'all', "testing {}".format(debug,))
     def test_notify(self):
-        ui.log.debug('>>> Inside function test_notify()')
+        ui.log.info('>>> Inside function test_notify()')
         from tool.utilities import get_configurations
         user = get_configurations('USER', 'name')
         expected = ' ({}) Notify'.format(user, )
@@ -144,7 +146,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'home' or debug is 'all', "testing {}".format(debug,))
     def test_home(self):
-        ui.log.debug('>>> Inside function test_home()')
+        ui.log.info('>>> Inside function test_home()')
         expected = "Let's take a look at how to navigate "
         result = self.process.results(expected)
         self.assertTrue(result, msg=expected)
@@ -152,7 +154,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'wiki' or debug is 'all', "testing {}".format(debug,))
     def test_wiki(self):
-        ui.log.debug('>>> Inside function test_wiki()')
+        ui.log.info('>>> Inside function test_wiki()')
         from ui.low.wiki import Wiki
         Wiki()
         expected = 'Full View'
@@ -166,7 +168,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'license' or debug is 'all', "testing {}".format(debug,))
     def test_license(self):
-        ui.log.debug('>>> Inside function test_license() landing page')
+        ui.log.info('>>> Inside function test_license() landing page')
         from ui.low.license import License
         License()
         expected = 'License Request'
@@ -176,7 +178,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'reports' or debug is 'all', "testing {}".format(debug,))
     def test_license_reports(self):
-        ui.log.debug('>>> Inside function test_license_reports()')
+        ui.log.info('>>> Inside function test_license_reports()')
         from ui.low.license_report import LicenseReport
         LicenseReport()
         expected = 'License Report'
@@ -186,7 +188,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'expire' or debug is 'all', "testing {}".format(debug,))
     def test_license_expire(self):
-        ui.log.debug('>>> Inside function test_license_expire()')
+        ui.log.info('>>> Inside function test_license_expire()')
         from ui.low.license_expire import LicenseExpire
         LicenseExpire()
         expected = 'Expiring Licenses'
@@ -196,7 +198,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'requirements' or debug is 'all', "testing {}".format(debug,))
     def test_license_requirements(self):
-        ui.log.debug('>>> Inside function test_license_requirements()')
+        ui.log.info('>>> Inside function test_license_requirements()')
         from ui.low.license_requirements import LicenseRequirements
         LicenseRequirements()
         expected = 'Manage State License Requirements'
@@ -206,7 +208,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'sales_report' or debug is 'all', "testing {}".format(debug,))
     def test_sales_commission_report(self):
-        ui.log.debug('>>> Inside function test_sales_commission_report()')
+        ui.log.info('>>> Inside function test_sales_commission_report()')
         from ui.low.sales_commission_report import SalesCommissionReport
         SalesCommissionReport()
         expected = 'Commission Report'
@@ -216,7 +218,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'goals' or debug is 'all', "testing {}".format(debug,))
     def test_sales_corporate_goals(self):
-        ui.log.debug('>>> Inside function test_sales_corporate_goals()')
+        ui.log.info('>>> Inside function test_sales_corporate_goals()')
         from ui.low.sales_corporate import SalesCorporate
         SalesCorporate()
         expected = 'Goals'
@@ -226,7 +228,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'rates' or debug is 'all', "testing {}".format(debug,))
     def test_sales_commission_rates(self):
-        ui.log.debug('>>> Inside function test_sales_commission_rates()')
+        ui.log.info('>>> Inside function test_sales_commission_rates()')
         from ui.low.sales_commission_rates import SalesCommissionRates
         SalesCommissionRates()
         expected = 'Commission Rates'
@@ -236,7 +238,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'base' or debug is 'all', "testing {}".format(debug,))
     def test_sales_base(self):
-        ui.log.debug('>>> Inside function test_sales_base()')
+        ui.log.info('>>> Inside function test_sales_base()')
         from ui.low.sales_base import SalesBase
         SalesBase()
         expected = 'Standard Sales Base'
@@ -246,7 +248,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'file' or debug is 'all', "testing {}".format(debug,))
     def test_file(self):
-        ui.log.debug('>>> Inside function test_file()')
+        ui.log.info('>>> Inside function test_file()')
         from ui.low.file import File
         from tool.utilities import get_configurations
         File()
@@ -258,7 +260,7 @@ class BvtBasic(unittest.TestCase):
     # @unittest.skipUnless(
     #     debug is 'watch' or debug is 'all', "testing {}".format(debug,))
     # def test_watch(self):
-    #     ui.log.debug('>>> Inside function test_watch()')
+    #     ui.log.info('>>> Inside function test_watch()')
     #     from ui.low.watch import Watch
     #     Watch()
     #     expected = ''  # Unknown
@@ -268,7 +270,7 @@ class BvtBasic(unittest.TestCase):
     # @unittest.skipUnless(
     #     debug is 'manage' or debug is 'all', "testing {}".format(debug,))
     # def test_watch_manage(self):
-    #     ui.log.debug('>>> Inside function test_watch_manage()')
+    #     ui.log.info('>>> Inside function test_watch_manage()')
     #     from ui.low.watch_manage import WatchManage
     #     WatchManage()
     #     expected = ''  # Unknown
@@ -278,7 +280,7 @@ class BvtBasic(unittest.TestCase):
     @unittest.skipUnless(
         debug is 'user' or debug is 'all', "testing {}".format(debug,))
     def test_user_config(self):
-        ui.log.debug('>>> Inside function test_user_config()')
+        ui.log.info('>>> Inside function test_user_config()')
         from ui.low.user_config import UserConfig
         UserConfig()
         expected = 'User Information'
@@ -288,49 +290,49 @@ class BvtBasic(unittest.TestCase):
     # @unittest.skipUnless(debug is 'admin' or debug is 'all',
     #                      "testing {}".format(debug,))
     # def test_admin_home(self):
-    #     ui.log.debug('>>> Inside function test_admin_home()')
+    #     ui.log.info('>>> Inside function test_admin_home()')
     #
     # @unittest.skipUnless(debug is 'status' or debug is 'all',
     #                      "testing {}".format(debug,))
     # def test_admin_user_status(self):
-    #     ui.log.debug('>>> Inside function test_admin_user_status()')
+    #     ui.log.info('>>> Inside function test_admin_user_status()')
     #
     # @unittest.skipUnless(debug is 'actions' or debug is 'all',
     #                      "testing {}".format(debug,))
     # def test_admin_edit_actions(self):
-    #     ui.log.debug('>>> Inside function test_admin_edit_actions()')
+    #     ui.log.info('>>> Inside function test_admin_edit_actions()')
     #
     # @unittest.skipUnless(debug is 'policies' or debug is 'all',
     #                      "testing {}".format(debug,))
     # def test_admin_edit_policies(self):
-    #     ui.log.debug('>>> Inside function test_admin_edit_policies()')
+    #     ui.log.info('>>> Inside function test_admin_edit_policies()')
     #
     # @unittest.skipUnless(debug is 'acls' or debug is 'all',
     #                      "testing {}".format(debug,))
     # def test_admin_assign_acls(self):
-    #     ui.log.debug('>>> Inside function test_admin_assign_acls()')
+    #     ui.log.info('>>> Inside function test_admin_assign_acls()')
     #
     # @unittest.skipUnless(debug is 'dash' or debug is 'all',
     #                      "testing {}".format(debug,))
     # def test_admin_dash(self):
-    #     ui.log.debug('>>> Inside function test_admin_dash()')
+    #     ui.log.info('>>> Inside function test_admin_dash()')
     #
     # @unittest.skipUnless(debug is 'send' or debug is 'all',
     #                      "testing {}".format(debug,))
     # def test_admin_send_notification(self):
-    #     ui.log.debug('>>> Inside function test_admin_send_notification()')
+    #     ui.log.info('>>> Inside function test_admin_send_notification()')
     #
     # @unittest.skipUnless(debug is 'namelist' or debug is 'all',
     #                      "testing {}".format(debug,))
     # def test_admin_namelist(self):
-    #     ui.log.debug('>>> Inside function test_admin_namelist()')
+    #     ui.log.info('>>> Inside function test_admin_namelist()')
     #
     # @unittest.skipUnless(debug is 'template' or debug is 'all',
     #                      "testing {}".format(debug,))
     # def test_admin_template_creator(self):
-    #     ui.log.debug('>>> Inside function test_admin_template_creator()')
+    #     ui.log.info('>>> Inside function test_admin_template_creator()')
     #
     # @unittest.skipUnless(debug is 'checklist' or debug is 'all',
     #                      "testing {}".format(debug,))
     # def test_admin_checklist_creator(self):
-    #     ui.log.debug('>>> Inside function test_admin_checklist_creator()')
+    #     ui.log.info('>>> Inside function test_admin_checklist_creator()')
