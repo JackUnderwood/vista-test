6cbbe7739738051b532e5e9abaf5f15a7ba38281
JackUnderwood <john.underwood@vistastaff.com>
2016-03-22 07:56:12 -0600

    Add new regression case for an inactive requirement - story 113028615.

---
+++ case/regression/requirement_inactive.py
@@ -0,0 +1,44 @@
+from ui import UI
+from ui.low.license import License
+from ui.high.checklist import Checklist
+
+__author__ = 'John Underwood'
+
+
+class RequirementInactive(UI):
+    """
+    Regression test for story #113028615
+    """
+    License()
+    Checklist()
+
+    runtime = {
+        'checklist': ('Click', 'css=#content>div.row>div.col.s3>ul>div>div>a'),
+        'manage': (
+            'Click',
+            '//*[@id="checklist-form-container"]/div[3]/div[5]/a/i'),
+        'active': (
+            'Click',
+            '//*[@id="stateLicenseRequirementEdit_form"]/div[1]/div[2]'
+            '/label/span'),
+        'save': (
+            'Click', '//*[@id="stateLicenseRequirementEdit_form"]/div[5]/a[2]')
+    }
+    process = UI()
+    process.update(runtime)
+    order = ('checklist', )
+    process.execute(order)
+
+    # Get the first Requirement's label
+    initial = process.get(
+        '//*[@id="checklist-form-container"]/div[3]/div[1]/label',
+        'innerHTML', ).strip()
+    # Set the first Requirement to Inactive
+    order = ('manage', 'active', 'save', )
+    process.execute(order)
+    # Comparison should NOT match
+    # Add a new Requirement
+    # Test case: check to see if the Inactive Requirement becomes active
+    # Set Requirements back to the 'Active' state
+    process.wait(5)
+    process.teardown()
