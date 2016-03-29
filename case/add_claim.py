40bf0e7f065649535b43924eb70dc53b0b4007f9
JackUnderwood <john.underwood@vistastaff.com>
2016-03-29 09:44:47 -0600

    Repaired broken links to Malp Claim.

--- case/add_claim.py
+++ case/add_claim.py
@@ -19,7 +19,7 @@ class AddClaim(UI):
 
     runtime = {
         'malpractice': ('Click', '//*[@id="content"]/div[2]/div[1]/ul/div/a[4]'),
-        'addClaim': ('Click', '//*[@id="malpracticeGrid_form"]/a[1]'),
+        'addClaim': ('Click', '//*[@id="malpracticeGrid_form_inline"]/a[1]'),
         'findInsuranceProvider': ('Type', '#insurance_provider_id_desc', 'ame'),
         'selectInsuranceProvider': ('Click', '#62963'),  # Orthopaedic Surgeons
         'status': ('Select', '#claim_status_id', 'Open'),
