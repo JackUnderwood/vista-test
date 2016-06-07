4fbd9cdb09c722eb6437f0219d7a57a55d3a1308
JackUnderwood <john.underwood@vistastaff.com>
2016-06-07 09:02:20 -0600

    Add new ui.low.JobsEdit class for the Job Board feature.

---
+++ ui/low/jobs_edit.py
@@ -0,0 +1,32 @@
+from ui import UI
+from tool.vlog import VLog
+__author__ = 'John Underwood'
+
+
+class JobsEdit(UI):
+    """
+    Begins at the main page and branches to the Licensing landing page.
+    """
+    def __init__(self, override=None):
+        super().__init__()
+        log = VLog(name="vtf", log_name="JOBS")
+        log.info("License __init__() called")
+        runtime = {
+            'level': '4',
+            'job':  ("Chain", [
+                ('move_to_element', {
+                    'to_element':
+                        '//*[@id="slide-out"]/li[&level;]/ul/li/a/i[1]'}),
+                ('click', {
+                    'on_element':
+                        '//*[@id="slide-out"]/li[&level;]/ul/li/a/i[1]'}),
+                ('click', {
+                    'on_element':
+                        '//*[@id="slide-out"]/li[&level;]/ul/li/div/ul/li[3]/a'}
+                 ),
+            ]),
+        }
+        process = UI(override)
+        process.update(runtime)
+        order = ('job', )
+        process.execute(order)
