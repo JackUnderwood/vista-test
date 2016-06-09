fc9f83d4d1992666017eeb064eb6f68e292a2486
JackUnderwood <john.underwood@vistastaff.com>
2016-06-09 10:15:12 -0600

    Added comparison between expected subtitle and actual subtitle; still need to clean up type's clear().

--- case/job_edit.py
+++ case/job_edit.py
@@ -10,16 +10,18 @@ class JobEdit(UI):
     JobSearch()
 
     runtime = {
+        'subtitleText': "Get Started Right Away",
         'text': "Some text",
-        'subtitle': ('Type', '#job_board_subtitle', 'Get Started Right Away'),
+        'subtitle': ('Type', '#job_board_subtitle', '&subtitleText;'),
         'template': ('Select', '#template', 'Marketing Tab'),
         'description': ('Type', '.cke_wysiwyg_frame', '&text;'),
     }
-    # expected = "<REQUIRED>"
+    expected = runtime['subtitleText']
     process = UI()
     process.update(runtime)
     order = ('subtitle', 'template', 'description', )
     process.execute(order)
-    # process.results(expected)
-    process.wait(3)
+    actual = process.get('css=#content>div>div:nth-child(2)>div>p.subtitle')
+    process.compare(expected, actual)
+    process.wait(1)
     process.teardown()
