c4c598fff11cb47df0d7bd6cd38223aff81523fc
JackUnderwood <john.underwood@vistastaff.com>
2016-10-06 11:26:48 -0600

    Fixed refactor of get to spy inside UI's chain() method--changed it back to selenium's get().

--- ui/__init__.py
+++ ui/__init__.py
@@ -311,7 +311,7 @@ class UI:
         for elem in elements:
             action, params = elem
             for k in params:  # only one element should be in dict at this point
-                sub_locator = params.spy(k, None)
+                sub_locator = params.get(k, None)
                 # The first argument 'check_chain' is a phony command;
                 # a command is not necessary for chaining.
                 replacer = self.check_for_placeholder(sub_locator)
