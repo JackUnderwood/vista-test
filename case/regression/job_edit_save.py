012d67df0d4c6f4d8b30c388ea6c5fb093cd8b15
JackUnderwood <john.underwood@vistastaff.com>
2017-01-26 14:36:51 -0700

    New case for Edit Job's Save button is inactive before any changes.

---
+++ case/regression/job_edit_save.py
@@ -0,0 +1,44 @@
+import ui
+from ui import UI
+from ui.low.job_posts import JobPosts
+__author__ = 'John Underwood'
+
+
+class JobEditSave(UI):
+    """
+    Regression test for story #129543903 - Edit Job drawer's Save button
+    """
+    ui.log.info("JobEditSave __init__() called")
+    JobPosts()
+
+    runtime = {
+        'search': ('Type', '#s_job_number', '90000-92150'),
+        'refresh': ('Click', '//*[@id="job-search-wrap"]/div[2]/div[2]/button')
+    }
+    process = UI()
+    process.update(runtime)
+    order = ('search', 'refresh', )
+    process.execute(order)
+
+    # Get the first row's job number.
+    job_number = process.spy(
+        '//*[@id="result-target"]/tbody/tr[1]/td[1]', 'innerHTML')
+    ui.log.info("JobNumber: {}".format(job_number, ))
+
+    edit_button_id = '#edit_{}'.format(job_number, )
+
+    runtime = {
+        'edit': ('Click', edit_button_id),
+    }
+    process.update(runtime)
+    order = ('edit',)
+    process.execute(order)
+    process.wait_for_element(
+        '#JobDescriptionTemplates__job_description_template_id', wait_time=10)
+    value_in_class = process.spy('#edit-save', 'class')
+    ui.log.info("RESULT: {}".format(value_in_class, ))
+
+    actual = 'disabled' in value_in_class
+    process.compare(True, actual, 'Is the Save button "disabled"?')
+    process.wait(5)
+    process.teardown()
