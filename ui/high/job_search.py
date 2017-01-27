e2697ffce27ff3fb21abd512ae84b44132c643e0
JackUnderwood <john.underwood@vistastaff.com>
2017-01-27 09:31:09 -0700

    Altered key names, and added comments.

--- ui/high/job_search.py
+++ ui/high/job_search.py
@@ -4,17 +4,25 @@ __author__ = 'John Underwood'
 
 
 class JobSearch(UI):
+    """
+    Search for a job number or a range of numbers, such as...
+        - a single job number: 54274
+        - a range of job numbers: 90000-92150
+        - an implied range:
+            - 90000-5100 same as 90000-95100
+            - 90000-100  same as 90000-90100
+    """
 
     def __init__(self, override=None):
         super().__init__()
         runtime = {
-            'num': '54274',  # override={'number': '54274'}
-            'job': ('Type', '#s_job_number', '&num;'),
-            'search': (
+            'value': '12345',  # override={'value': '12345'}
+            'search': ('Type', '#s_job_number', '&value;'),
+            'refresh': (
                 'Click',
                 '//*[@id="job-search-wrap"]/div[2]/div[2]/button', ),
         }
         process = UI(override)
         process.update(runtime)
-        order = ('job', 'search', )
+        order = ('search', 'refresh', )
         process.execute(order)
