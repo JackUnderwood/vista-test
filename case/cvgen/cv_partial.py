0b125e2dc52721a200da5d337ede6c366cca153b
JackUnderwood <john.underwood@vistastaff.com>
2017-04-07 10:56:36 -0600

    Fixed FileCvPartial to use variable for generated string.

--- case/cvgen/cv_partial.py
+++ case/cvgen/cv_partial.py
@@ -7,17 +7,18 @@ __author__ = 'John Underwood'
 
 class CvPartial(UI):
     FileCvPartial()
-    random_value = gen_key(6)
+    partial_name = 'auto_edu_'
+    partial_random = gen_key(6)
 
     runtime = {
         'create': ('Click', '//*[@id="template-list"]/ul/li[1]/a', ),
-        'partialName': ('Type', '#partials_name', 'auto_edu' + random_value),
+        'partialName': ('Type', '#partials_name', partial_name + partial_random),
         'category': ('Select', '#partials_category', 'Education'),
         'placeholder': ('Click', '//*[@id="control-list"]/ul/li[9]/a', ),
         'save': ('Click', '#savePartial', ),
     }
 
-    expected = 'auto_' + random_value
+    expected = partial_name + partial_random
     process = UI()
     order = ('create', 'partialName', 'category', 'placeholder')
     process.update(runtime)
