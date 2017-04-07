0272969969a8528edd71522e4b3eadeb32aa1811
JackUnderwood <john.underwood@vistastaff.com>
2017-04-07 09:51:47 -0600

    Added new test case for CV Partials; saves a new partial to the Available list.

---
+++ case/cvgen/cv_partial.py
@@ -0,0 +1,34 @@
+from ui import UI
+from ui.low.file_cvpartial import FileCvPartial
+from tool.generators.generator import gen_key
+
+__author__ = 'John Underwood'
+
+
+class CvPartial(UI):
+    FileCvPartial()
+    random_value = gen_key(6)
+
+    runtime = {
+        'create': ('Click', '//*[@id="template-list"]/ul/li[1]/a', ),
+        'partialName': ('Type', '#partials_name', 'auto_edu' + random_value),
+        'category': ('Select', '#partials_category', 'Education'),
+        'placeholder': ('Click', '//*[@id="control-list"]/ul/li[9]/a', ),
+        'save': ('Click', '#savePartial', ),
+    }
+
+    expected = 'auto_' + random_value
+    process = UI()
+    order = ('create', 'partialName', 'category', 'placeholder')
+    process.update(runtime)
+    process.execute(order)
+    process.wait(1)
+
+    order = ('save', )
+    process.execute(order)
+    process.wait(1)
+
+    process.results(expected, locator='//*[@id="template-list"]/ul')
+
+    process.wait(1)
+    process.teardown()
