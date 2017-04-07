cac909d04eac555f325f0c3ffe273d0ac9cb1e0a
JackUnderwood <john.underwood@vistastaff.com>
2017-04-07 11:20:41 -0600

    Simplified the 'expected' string.

--- case/cvgen/cv_partial.py
+++ case/cvgen/cv_partial.py
@@ -1,4 +1,5 @@
 from ui import UI
+
 from ui.low.file_cvpartial import FileCvPartial
 from tool.generators.generator import gen_key
 
@@ -7,18 +8,17 @@ __author__ = 'John Underwood'
 
 class CvPartial(UI):
     FileCvPartial()
-    partial_name = 'auto_edu_'
-    partial_random = gen_key(6)
+    partial_name = 'auto_edu_' + gen_key(6)
 
     runtime = {
         'create': ('Click', '//*[@id="template-list"]/ul/li[1]/a', ),
-        'partialName': ('Type', '#partials_name', partial_name + partial_random),
+        'partialName': ('Type', '#partials_name', partial_name),
         'category': ('Select', '#partials_category', 'Education'),
         'placeholder': ('Click', '//*[@id="control-list"]/ul/li[9]/a', ),
         'save': ('Click', '#savePartial', ),
     }
 
-    expected = partial_name + partial_random
+    expected = partial_name
     process = UI()
     order = ('create', 'partialName', 'category', 'placeholder')
     process.update(runtime)
