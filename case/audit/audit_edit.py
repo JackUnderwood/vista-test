3cc39da8e9a92b5e02e7d8e5674fb8f6b8deedb7
JackUnderwood <john.underwood@vistastaff.com>
2017-04-19 13:13:56 -0600

    Created test case for editing an audit configuration.

--- case/audit/audit_edit.py
+++ case/audit/audit_edit.py
@@ -7,6 +7,18 @@ class AuditEdit(UI):
     process = UI()
     process.go_to_driver_url("http://indytest/auditConfig")
 
+    runtime = {
+        'edit': ('Click', '//*[@id="edit_902_17"]/i'),
+        'option': ('Click',
+                   '//*[@id="editExisting"]/div[1]/div[4]/div[2]/label[38]'),
+        'save': ('Click', '#saveButton')
+    }
+    expected = 'Audit Conditions Saved Successfully.'
+    process.update(runtime)
+    order = ('edit', 'option', 'save')
+    process.execute(order)
+    actual = process.spy('//*[@id="editExisting"]/div[1]/p', 'innerHTML')
+    process.compare(expected, actual)
+
     process.wait(1)
     process.teardown()
-    pass
