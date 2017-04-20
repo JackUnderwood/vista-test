858fe4d3ac9f81ca13c3713818d5130287605fd6
JackUnderwood <john.underwood@vistastaff.com>
2017-04-20 10:18:12 -0600

    Added new test case for delete_audit_condition.

--- case/audit/delete_audit_condition.py
+++ case/audit/delete_audit_condition.py
@@ -8,12 +8,12 @@ class DeleteAuditCondition(UI):
     Create an audit configuration, then delete it.
 
     Click on the audit config's delete button for a successful delete.
+    Note: incomplete test case; currently unable to successfully delete
+    audit conditions inside audit - #143717105 get 403 error--missing acl
     """
     process = UI()
     process.get("auditConfig")
     user = 'Angie King'
-    # //*[@id="auditConfig_grid"]/tbody/tr[2]/td[1]
-    locator_id = process.spy('css=.hiddendiv', 'innerHTML')
 
     runtime = {
         'add': ('Click', '#newAuditCondition'),
@@ -24,11 +24,39 @@ class DeleteAuditCondition(UI):
         'address2': ('Click', '//*[@id="fields_1"]/div[2]/label[2]'),
         'city':     ('Click', '//*[@id="fields_1"]/div[2]/label[7]'),
         'save': ('Click', '#saveButton'),
-        'delete': ('Click', '//*[@id="remove_902_17"]/i'),
+        'close': ('Click', '#closeButton'),
     }
     process.update(runtime)  # create a new audit condition
     order = ('add', 'searchUser', 'selectUser', 'table',
-             'address1', 'address2', 'city', 'save')
+             'address1', 'address2', 'city', 'save', 'close')
+    process.execute(order)
+    process.wait(1)
+
+    # Spy into the first element's row, and get its unique id
+    # outerHTML: <td class=" hiddendiv">902_17</td>
+    # e.g. locator_id = 902_17
+    locator_id = process.spy('//*[@id="auditConfig_grid"]/tbody/tr[1]/td[1]',
+                             'innerHTML')
+
+    runtime.update({
+        'delete': ('Click', '//*[@id="remove_{}"]/i'.format(locator_id,)),
+    })
+    expected = 'Are you sure you wish to remove audits'
+    process.update(runtime)
+    order = ('delete', )
     process.execute(order)
 
+    found = process.results(expected)
+    if found:
+        runtime.update({
+            'continue': ('Click', '#confirmButton'),
+        })
+        # expected = '<unsure; not functioning at time of case development>'
+        process.update(runtime)
+        order = ('continue', )
+        process.execute(order)
+        # process.results(expected)
+
+    process.wait(3)
+    process.teardown()
 
