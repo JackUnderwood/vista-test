8e94113396f37226418a6f0bab2a86f4f13453eb
JackUnderwood <john.underwood@vistastaff.com>
2017-04-25 08:18:46 -0600

    Completed unittest for test_division().

--- case/jobpost/suite_all_job_search.py
+++ case/jobpost/suite_all_job_search.py
@@ -39,6 +39,20 @@ class AllJobSearch(unittest.TestCase):
         debug is 'ard' or debug is 'all', "testing {}".format(debug,))
     def test_ard_group(self):
         ui.log.info('>>> Inside function test_ard_group()')
+        runtime = {
+            'ard': ('Click', 'css=.ui-multiselect.ui-widget.ui-state-default.'
+                             'ui-corner-all.multi_s.multi_s_division'),
+            'domesticLT': ('Click', '#ui-multiselect-s_division-option-3'),
+            'hcp': ('Click', '#ui-multiselect-s_division-option-4'),
+        }
+        self.process.update(runtime)
+        order = ('ard', 'domesticLT', 'hcp', 'ard', )
+        self.process.execute(order)
+        self.process.wait()
+        html = self.process.spy('//*[@id="result-target"]/tbody', 'innerHTML')
+        compare_message = ('the result-target is not empty' if html != '' else
+                           'the result-target is empty')
+        self.process.compare(html != '', True, message=compare_message)
 
     @unittest.skipUnless(
         debug is 'division' or debug is 'all', "testing {}".format(debug,))
