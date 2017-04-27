75a7e84d2a7fae2d8c16b1fc6eab449dcbf3da5d
JackUnderwood <john.underwood@vistastaff.com>
2017-04-27 14:19:35 -0600

    Added new test case for correspond template creator; saves small change to the template's subject.

---
+++ case/admin/save_corr_template.py
@@ -0,0 +1,34 @@
+from ui import UI
+from ui.low.corr_template_creator import CorrTemplateCreator
+from tool.generators.generator import gen_key
+
+__author__ = 'John Underwood'
+
+
+class SaveCorrTemplate(UI):
+    """
+    Save small changes to an existing correspond template on the 
+    Create Template page.
+    """
+    CorrTemplateCreator()
+    template_id = '16'  # License Renewal on test server has id of 16
+    unique_key = gen_key(4)
+    runtime = {
+        'template': (
+            'Click', '//*[@id="templatelist"]/li[{}]/div'.format(template_id, )),
+        'subject': (
+            'TypeAndTab',
+            '#CorrespondenceTemplateEmailSubject',
+            'License Renewal - VISTA {}'.format(unique_key, )),
+        'save': ('Click', 'css=.waves-effect.waves-light.btn.template-save.'
+                          'cyan.darken-3'),
+    }
+    expected = 'Saved Template'
+    process = UI()
+    process.update(runtime)
+    process.execute(('template', 'subject', 'save',))
+    process.wait()
+
+    process.results(expected, locator='toast-container')
+    process.wait()
+    process.teardown()
