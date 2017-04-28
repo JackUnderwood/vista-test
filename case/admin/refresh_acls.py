921a7aa5f34f79c6af3c47a0196caa087c13e98b
JackUnderwood <john.underwood@vistastaff.com>
2017-04-28 14:05:33 -0600

    Added case that goes to the User Status page and clicks the Refresh button; no results are possible as of yet.

---
+++ case/admin/refresh_acls.py
@@ -0,0 +1,25 @@
+from ui import UI
+from ui.low.admin_user_status import AdminUserStatus
+
+__author__ = 'John Underwood'
+
+
+class RefreshAcls(UI):
+    """
+    Navigate to Administrator | User Status and click the 
+    "Refresh All Users' ACLs" button
+    """
+    process = UI()
+    AdminUserStatus()
+
+    runtime = {
+        'refresh': ('Click', '#reload-all-users')
+    }
+    expected = 'Automatically reloaded account permissions'
+    process.update(runtime)
+    process.execute(('refresh', ))
+    process.wait(2)
+    # Getting an unknown selenium error when refreshing acls and then trying
+    # to use the web driver--it's possible that the driver object was changed
+    # process.results(expected)  # locator='toast-green')
+    process.teardown()
