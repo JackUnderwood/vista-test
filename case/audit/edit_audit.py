c9911e3579012df76523f47c9fd99609197207ac
JackUnderwood <john.underwood@vistastaff.com>
2017-05-02 13:52:41 -0600

    Removed edit dependency, so audit config edit is a self contained test case; added a config and then do edit.

--- case/audit/edit_audit.py
+++ case/audit/edit_audit.py
@@ -9,11 +9,35 @@ class EditAudit(UI):
     """
     process = UI()
     process.get("auditConfig")
+    user = 'Angie King'
 
     runtime = {
-        'edit': ('Click', '//*[@id="edit_902_17"]/i'),
+        'add': ('Click', '#newAuditCondition'),
+        'searchUser': ('Type', '#userDescription', user),
+        'selectUser': ('Click', '//*[@item_id="1001"]'),
+        'table': ('Select', '#historyTablesId', 'address'),
+        'address1': ('Click', '//*[@id="fields_1"]/div[2]/label[1]'),
+        'address2': ('Click', '//*[@id="fields_1"]/div[2]/label[2]'),
+        'city':     ('Click', '//*[@id="fields_1"]/div[2]/label[7]'),
+        'save': ('Click', '#saveButton'),
+        'close': ('Click', '#closeButton'),
+    }
+    process.update(runtime)  # create a new audit condition
+    order = ('add', 'searchUser', 'selectUser', 'table',
+             'address1', 'address2', 'city', 'save', 'close')
+    process.execute(order)
+    process.wait(1)
+
+    # Spy into the first result's row, and get its unique id
+    # Outer HTML: <td class=" hiddendiv">902_17</td>
+    # e.g. locator_id = 902_17
+    locator_id = process.spy('//*[@id="auditConfig_grid"]/tbody/tr[1]/td[1]',
+                             'innerHTML')
+
+    runtime = {
+        'edit': ('Click', '//*[@id="edit_{}"]/i'.format(locator_id, )),
         'option': ('Click',
-                   '//*[@id="editExisting"]/div[1]/div[4]/div[2]/label[38]'),
+                   '//*[@id="editExisting"]/div[1]/div[4]/div[2]/label[18]'),
         'save': ('Click', '#saveButton')
     }
     expected = 'Audit Conditions Saved Successfully.'
