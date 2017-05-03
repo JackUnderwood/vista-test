ac5b04dd3442ca5f8e2ba96ff4f20a7357b41120
JackUnderwood <john.underwood@vistastaff.com>
2017-05-03 15:48:05 -0600

    Check for address validation dialog box during save process.

--- case/edit_entity.py
+++ case/edit_entity.py
@@ -33,13 +33,21 @@ class EditEntity(UI):
         # 123 N Main St, Lindon, UT 84042
         # has a class called "toast red"
     }
-    expected = 'Saved information'
     process = UI()
+    expected = 'Saved information'
     process.update(runtime)
     order = ('editEntity', 'addressDescription', 'addressType',
              'address', 'city', 'state', 'zipCode', 'emailType',
              'save', )  # 'expectedError'
     process.execute(order)
-    process.results(expected, locator='toast-container', wait_time=10)
-    process.wait(3)
+    validation_check = process.get_source()
+    if "address check" in validation_check:
+        process.update({
+            'saveAsIs': ('Click', 'css=.waves-effect.waves-light.btn.'
+                                  'right-align.modal-action.modal-close')
+        })
+        process.execute(('saveAsIs', ))
+    process.wait()
+    process.results(expected, locator='toast-container', wait_time=20)
+    process.wait()
     process.teardown()
