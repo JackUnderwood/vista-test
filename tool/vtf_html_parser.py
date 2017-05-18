7336376471e1436ba9be7221f6a9735f70440e7b
JackUnderwood <john.underwood@vistastaff.com>
2017-05-18 12:49:15 -0600

    Fixed index positions adding empty [] lists outside of the embedded list.

--- tool/vtf_html_parser.py
+++ tool/vtf_html_parser.py
@@ -6,23 +6,31 @@ __author__ = 'John Underwood'
 class VtfHtmlParser(HTMLParser):
     data = []
     _sub = {}
+    _index = 0
     _count = 0
 
     def handle_starttag(self, tag, attributes):
         self._sub = {}
-        self.data.append([])
-        self.data[self._count].append(tag)
+        if self._count is 0:
+            self.data.append([])
+        self._count += 1
+        self.data[self._index].append(tag)
         # print("Start tag:", tag)
         for attr in attributes:
             # print("     attr:", attr)
             self._sub[attr[0]] = attr[1]
-        self.data[self._count].append(self._sub)
+        self.data[self._index].append(self._sub)
 
     def handle_endtag(self, tag):
-        # print("End tag  :", tag)
-        # print("SUB_DATA: {}".format(self.sub, ))
-        # print("COUNT: {}".format(self.count, ))
-        self._count += 1
+        # print("END TAG :", tag)
+        # print("SUB     : {}".format(self._sub, ))
+        self._count -= 1
+        if self._count < 0:
+            self._count = 0
+        if self._count == 0:
+            self._index += 1
+        # print("INDEX: {}".format(self._index, ))
+        # print("COUNT: {}".format(self._count, ))
 
     def handle_data(self, data):
         # print("Data     :", data)
