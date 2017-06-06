f080f5b860c55c3f01c32b607c7eb6a53251ba19
JackUnderwood <john.underwood@vistastaff.com>
2017-06-06 11:57:36 -0600

    Completed test case for JobEdit; finds all white rows, and saves required changes.

--- case/jobpost/job_edit.py
+++ case/jobpost/job_edit.py
@@ -1,29 +1,63 @@
 from ui import UI
 from ui.low.job_posts import JobPosts
-from ui.high.job_search import JobSearch
+from ._helpers import find_rows
 
 __author__ = 'John Underwood'
 
 
 class JobEdit(UI):
-    job_number = '90000'
+    """
+    Fill in necessary elements using incomplete (white) job and save the
+    filled job.
+    """
     JobPosts()
-    JobSearch(override={'value': job_number})
 
     runtime = {
-        'subtitleText': "Get Started Right Away",
-        'descText': "Some text",
-        'edit': ('Click', '#edit_' + job_number,),
-        'subtitle': ('Type', '#jobs__job_board_subtitle', '&subtitleText;'),
-        'template': ('Select', '#template', 'Marketing Tab'),
-        'description': ('TypeInCkeditor', '.cke_wysiwyg_frame', '&descText;'),
+        'JobStatus': ('Click', 'css=.ui-multiselect.ui-widget.ui-state-default.'
+                               'ui-corner-all.multi_s.multi_s_job_status'),
+        'wait': ('Wait', '#ui-multiselect-s_job_status-option-1',
+                 {'condition': 'element_to_be_clickable', 'wait_time': '2'}),
+        'JobStatusActive': (
+            'Click', '#ui-multiselect-s_job_status-option-1'),
+        'JobStatusHot': (
+            'Click', '#ui-multiselect-s_job_status-option-4'),
     }
-    expected = runtime['subtitleText']
     process = UI()
     process.update(runtime)
-    order = ('edit', 'subtitle', 'description', )
+    order = ['JobStatus', 'wait', 'JobStatusActive', 'JobStatusHot', ]
     process.execute(order)
-    actual = process.spy('#jobs__job_board_subtitle', 'value')
-    process.compare(expected, actual)
+    process.wait()
+
+    # Get only the 'white' rows--rows with no status set.
+    rows = process.find_elements(
+        '//*[@id="result-target"]/tbody/tr[@class=" " or @class="odd "]')
+    if not rows:
+        process.compare(True, False, message="no white row jobs available")
+    else:
+        row_ids = [row.find_element_by_xpath('./td[1]').text for row in rows]
+        job_number = row_ids.pop(0)
+
+        runtime = {
+            'subtitleText': "Get Started Right Away",
+            'descText': "Some text",
+            'edit': ('Click', '#edit_' + job_number,),
+            'subtitle': ('Type', '#jobs__job_board_subtitle', '&subtitleText;'),
+            'template': ('Select', '#template', 'Marketing Tab'),
+            'description': ('TypeInCkeditor', '.cke_wysiwyg_frame', '&descText;'),
+            'save': ('Click', '#edit-save')
+        }
+        expected = runtime['subtitleText']
+        process = UI()
+        process.update(runtime)
+        order = ('edit', 'subtitle', 'description', )
+        process.execute(order)
+        actual = process.spy('#jobs__job_board_subtitle', 'value')
+        process.compare(expected, actual)
+
+        expected = 'Job Saved'
+        process.execute(('save',))
+        process.wait()
+        process.results(expected)
+
     process.wait(1)
     process.teardown()
