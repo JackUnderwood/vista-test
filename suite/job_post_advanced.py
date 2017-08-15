1e03ed4e51b869bc2ad649dce450de6f6cd18260
JackUnderwood <john.underwood@vistastaff.com>
2017-08-15 13:50:19 -0600

    Created new suite with one test case for Advanced search; more test cases coming...

--- suite/job_post_advanced.py
+++ suite/job_post_advanced.py
@@ -37,5 +37,26 @@ class TestSuiteJobPostAdvanced(unittest.TestCase):
         :return: void
         """
         ui.log.info(">>> Inside function test_basic_single_positive()")
+        runtime = {
+            'group': (
+                'Select',
+                '//*[@id="adv-seach-prime"]/div/div[2]/select',
+                'EMR Types'),
+            'option': ('Select', '//*[@id="adv-seach-prime"]/div/div[4]/select',
+                       'Cerner'),
+            'search': ('Click', '//*[@id="job-search-wrap"]/div[3]/div[2]/button'),
+            'expand': ('Click', '//*[@id="result-target"]/thead/tr[1]/td[3]/i[1]'),
+        }
+        self.process.update(runtime)
+        self.process.execute(('group', 'option', 'search', ))
         self.process.wait()
-        pass
+        self.process.execute(('expand', ))
+        expected = 'EMR: Cerner'
+        job_number = self.process.spy(
+            '//*[@id="result-target"]/tbody/tr[1]/td[1]', 'innerHTML')
+        result = self.process.spy(
+            '//*[@id="expandable_{}"]/td/div/div[2]/div/div'.format(job_number),
+            'innerHTML')
+        self.process.compare(True, expected in result)
+        self.process.wait()
+        self.assertTrue(result, msg=expected)
