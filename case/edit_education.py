a1439512f272a60cacd99969058186565ae0466d
JackUnderwood <john.underwood@vistastaff.com>
2017-10-12 14:36:15 -0600

    Added the beginnings of new test case for Education drawer.

--- case/edit_education.py
+++ case/edit_education.py
@@ -1,2 +1,14 @@
+from ui import UI
 
 __author__ = 'John Underwood'
+
+
+class EditEducation(UI):
+    """ See TestRail test case C276 - Edit Education
+    1 - Click Education inside a provider's ribbon
+    2 - Click an education row's edit button
+    3 - Change a value in the Manage Education drawer's form, e.g. Degree Type
+    4 - Save the changes
+    Expected: Education saved successfully, and row shows new changed value
+    """
+    pass
