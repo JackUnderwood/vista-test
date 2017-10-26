013c63a865ffcc0407fe7053edee81ba0cf6da7d
JackUnderwood <john.underwood@vistastaff.com>
2017-10-26 15:20:31 -0600

    Start EditContactNotes() test case.

---
+++ case/edit_contact_notes.py
@@ -0,0 +1,18 @@
+from ui import UI
+
+__author__ = 'John Underwood'
+
+
+class EditContactNotes(UI):
+    """ See TestRail test case C251 - Get to the Contact Notes panel
+    Precondition
+    1 - Click on Find... field in header, find a provider, and select
+    2 - Close the Workspace
+    Test case
+    3 - Open the Workspace
+    4 - Click on the Manage phone numbers icon.
+    5 - Click on a row's phone number button.
+    Expected: The Contact Notes drawer displays for provider from
+    precondition's step 1.
+    """
+    pass
