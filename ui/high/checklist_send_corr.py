cc0bead348579ee62785d1ca9750c40a185d021d
JackUnderwood <john.underwood@vistastaff.com>
2017-11-08 14:29:07 -0700

    Added new ui.high.checklist_send_corr class; clicks the Send Corr button and selects a template.

---
+++ ui/high/checklist_send_corr.py
@@ -0,0 +1,22 @@
+import ui
+from ui import UI
+
+__author__ = 'John Underwood'
+
+
+class ChecklistSendCorr(UI):
+
+    def __init__(self, override=None):
+        super().__init__()
+
+        runtime = {
+            'send': ('Click',
+                     '//*[@id="ribbon_form"]/ul/li/div[3]/div[4]/div[1]/a[1]/i'),
+            'template': ('Click',
+                         '//*[@id="correspondenceChooser_form"]/p[2]/p[10]/a'),
+        }
+        ui.log.info("Select the License Renewal template")
+        process = UI(override)
+        process.update(runtime)
+        process.execute(('send', 'template',))
+        process.wait(3)
