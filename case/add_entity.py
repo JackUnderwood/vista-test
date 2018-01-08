c152a4f1e2571144797c00aea298a2eec884c398
JackUnderwood <john.underwood@vistastaff.com>
2018-01-08 10:29:09 -0700

    Removed an element from the order tuple.

--- case/add_entity.py
+++ case/add_entity.py
@@ -27,7 +27,7 @@ class AddEntity(UI):
         'address1': ('Type', 'css=input#address_1.address_1', '275 E 200 S'),
         'address2': ('Type', 'css=input#address_2.address_2', ''),
         'city': ('Type', 'css=input#city.city', 'Salt Lake City'),
-        'state': ('Select', 'css=select#state.browser-default', 'Utah'),
+        'state': ('Select', '#state', 'Utah'),
         'zipcode': ('Type', 'css=input#zip_code.zip_code', '84111'),
         'email': ('Type', '#email_address', email),
         'emailType': ('Select', '#email_correspondence_method_type_id', 'Work'),
@@ -38,9 +38,12 @@ class AddEntity(UI):
     process = UI()
     process.update(runtime)
     order = ('experience', 'addExperience', 'addEntity', 'status', 'name',
-             'addressDesc', 'addressType', 'address1', 'address2', 'city',
-             'state', 'zipcode', 'email', 'emailType', 'save', )
+             'addressDesc', 'addressType', 'address1', 'city',  # 'state',
+             'zipcode', 'email', 'emailType', 'save', )
     process.execute(order)
+    # Note: Removed 'state' from order due to more than one element.
+    # elements = process.find_elements('#state')
+    # two - elements[1].tag_name == 'select' & elements[0].tag_name == 'input'
     process.results(expected, locator='toast-container', wait_time=5)
     process.wait(3)
     process.teardown()
